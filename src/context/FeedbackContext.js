8d1ba26023b595017ce055ac3b446663e233cd6d
Ediz <edizyurdakul@gmail.com>
2022-01-26 17:38:07 +0200

    Change update, delete according to json server for persistent data

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -15,7 +14,7 @@ export const FeedbackProvider = ({ children }) => {
 
   const fetchFeedback = async () => {
     const response = await fetch(
-      "https://feedback-app-json-server.herokuapp.com/feedback"
+      "https://feedback-app-json-server.herokuapp.com/feedback?_sort=id&_order=desc"
     );
     const data = await response.json();
     setFeedback(data);
@@ -23,13 +22,27 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Add Feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch(
+      "https://feedback-app-json-server.herokuapp.com/feedback",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newFeedback),
+      }
+    );
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   // Delete Feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
+    await fetch(
+      `https://feedback-app-json-server.herokuapp.com/feedback/${id}`,
+      { method: "DELETE" }
+    );
     setFeedback(feedback.filter((item) => item.id !== id));
   };
 
@@ -42,12 +55,23 @@ export const FeedbackProvider = ({ children }) => {
     setFeedbackEdit({ item, edit: true });
   };
 
-  const updateFeedback = (id, updatedItem) => {
+  // Update Feedback
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(
+      `https://feedback-app-json-server.herokuapp.com/feedback/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedItem),
+      }
+    );
+
+    const data = await response.json();
     if (feedbackEdit.edit === true) {
       setFeedback(
-        feedback.map((item) =>
-          item.id === id ? { ...item, ...updatedItem } : item
-        )
+        feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
       );
     }
   };
